efc885643bddb972956bf375d50eac7efc428059
crotl_000 <crotlj6@gmail.com>
2022-11-19 22:06:35 +0300

    Добавьте файлы проекта.

---
+++ Homework_39.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+
+#define big(x, y)(x > y ? x : y)
+#define two(x)(x % 2 == 0 ? "chet" : "neChet")
+
+int main()
+{
+    int x;
+    int y;
+    std::cout << "enter x:\n";
+    std::cin >> x;
+    std::cout << "enter y:\n";
+    std::cin >> y;
+    std::cout << big(x, y) << " is bigger\n" << x << " x is " << two(x);
+}
